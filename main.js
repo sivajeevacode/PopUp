d3afb700e3090705ae6a6a995e0e2577d46d80a2
SIVA <99714297+sivajeevacode@users.noreply.github.com>
2024-01-17 19:33:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var popup=document.getElementById("popup")
+
+//new class add
+function pop()
+{
+    popup.classList.add("new")
+}
+//new class remove
+function up()
+{
+    popup.classList.remove("new")
+}
